e2da18862e29df1eade154c760a60fccac4f6820
yanxw <yanxuwen@szy.cn>
2019-05-08 16:10:37 +0800

    ...

--- http-api/src/main/java/com/http/api/DataCallBack.java
+++ http-api/src/main/java/com/http/api/DataCallBack.java
@@ -1,25 +1,17 @@
 package com.http.api;
 
 
-import android.os.Handler;
-import android.os.Looper;
-
 import java.lang.reflect.Type;
 
 public abstract class DataCallBack<T> {
 
     private Type mType;
 
-    private Handler mHandler;
 
     public DataCallBack(Class<T> clazz) {
         mType = clazz;
-        mHandler = new Handler(Looper.getMainLooper());
     }
 
-    public DataCallBack() {
-        mHandler = new Handler(Looper.getMainLooper());
-    }
 
     public abstract void onHttpSuccess(T result);
     public abstract void onHttpFail(NetError netError);
@@ -29,41 +21,14 @@ public abstract class DataCallBack<T> {
      * @param t
      */
     public void postUISuccess(final T t,boolean syn) {
-        if (syn){
-            onHttpSuccess(t);
-        } else {
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        onHttpSuccess(t);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
+        onHttpSuccess(t);
     }
 
     /**
      * 失败-回调到UI线程
      */
     public void postUIFail(final NetError netError,boolean syn) {
-        if (syn){
-            onHttpFail(netError);
-        } else {
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        onHttpFail(netError);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
-
+        onHttpFail(netError);
     }
 
     public Type getType() {
